b58e459255848b9274b0912faeed88eda0e8d041
gio797 <119562363+gio797@users.noreply.github.com>
2023-07-14 09:27:52 +0400

    implement header about and subheading components

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper-info">
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className="app__header-h1">The key to Fine dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat
+        morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet
+        tellus{" "}
+      </p>
+      <button className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper-image">
+      <img src={images.welcome} alt="header image" />
+    </div>
   </div>
 );
 
